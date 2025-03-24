89eaa9ab08df248833affdbf2da0b1fddc0df3cd
ItzWeihao <weihaoandersen@gmail.com>
2025-03-24 16:10:54 +0100

    door interaction

---
+++ Assets/Scripts/Interaction/DoorInteraction_R.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class DoorInteraction_R : MonoBehaviour, IInteractable
+{
+    private new Transform transform;
+    private Quaternion doorStart;
+    private bool doorOpen;
+
+    private void Awake()
+    {
+        transform = GetComponent<Transform>();
+        doorStart = transform.rotation;
+        doorOpen = false;
+    }
+
+    public void Interact()
+    {
+        if (doorOpen == false)
+        {
+            transform.Rotate(0, doorStart.y + 75, 0);
+            doorOpen = true;
+        } else
+        {
+            transform.Rotate(0, 285 - doorStart.y , 0);
+            doorOpen = false;
+        }
+    }
+}
