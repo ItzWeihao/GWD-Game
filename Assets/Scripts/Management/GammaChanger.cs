72c4fe359d69b7cab876596499dcc1f02ce425bf
MrMarshmellow99 <Patrickbukholt@hotmail.dk>
2025-05-05 12:00:24 +0200

    Removed managers from most scenes

--- Assets/Scripts/Management/GammaChanger.cs
+++ Assets/Scripts/Management/GammaChanger.cs
@@ -1,25 +1,46 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
 using UnityEngine.Rendering.Universal;
+
 public class GammaChanger : MonoBehaviour
 {
     private LiftGammaGain gamma;
     private Volume volume;
-    public Slider _slider;
+    private GameObject _options;
     public Camera _camera;
+    [SerializeField] private Slider gammaSlider;
+
     private void Start()
     {
+        
         volume = GetComponent<Volume>();
-        volume.profile.TryGet<LiftGammaGain>(out gamma);
+        if (volume != null && volume.profile != null)
+        {
+            volume.profile.TryGet(out gamma);
+        }
+        
+        _options = GameObject.Find("=== SceneManager ===");
+        if (_options != null)
+        {
+            gammaSlider = _options.GetComponent<Slider>();
+        }
     }
+
     public void AdjustGamma(float value)
     {
         Debug.Log("Slider Value: " + value);
-        gamma.gamma.value = new Vector4(1f, 1f, 1f, value);
 
+        // Null-check before accessing gamma
+        if (gamma != null)
+        {
+            gamma.gamma.value = new Vector4(1f, 1f, 1f, value);
+        }
+        else
+        {
+            Debug.LogWarning("Gamma component is missing or not assigned.");
+        }
     }
 }
